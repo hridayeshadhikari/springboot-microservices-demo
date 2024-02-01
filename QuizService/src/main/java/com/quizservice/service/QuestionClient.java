910c7e7d652a499e20fbe91f841898c5a5df5c6b
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-02-01 20:05:15 +0530

    added eureka server and load balancer dependency

--- QuizService/src/main/java/com/quizservice/service/QuestionClient.java
+++ QuizService/src/main/java/com/quizservice/service/QuestionClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8091",value = "Question-Client")
+//@FeignClient(url = "http://localhost:8091",value = "Question-Client")
+@FeignClient(name = "QUESTION-SERVICE")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
